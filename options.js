1661e80e1bd296b882f5e5929827e60da3c0ffc7
David <daviddanelian3@gmail.com>
2023-01-18 23:15:00 -0500

    Created server to save csv after option responses

--- options.js
+++ options.js
@@ -1,7 +1,37 @@
-document.getElementById("other").addEventListener("click", () => {
-    if(document.getElementById("other").checked) {
-        document.getElementById("other_txt").style.display = "block"
+// Styling for other checkbox
+document.getElementById('other').addEventListener('click', () => {
+    if(document.getElementById('other').checked) {
+        document.getElementById('other_txt').style.display = 'block';
     } else {
-        document.getElementById("other_txt").style.display = "none"
+        document.getElementById('other_txt').style.display = 'none';
     }
+});
+
+document.getElementById('submit').addEventListener('click', () => {
+    const apiURL = 'http://localhost:3000';
+    request = {
+        confidence: document.getElementById('confidence').checked,
+        confidenceTimestamp: 0,
+        guess: document.getElementById('q1').checked,
+        changedMind: document.getElementById('q2').checked,
+        mathMistake: document.getElementById('q3').checked,
+        other: document.getElementById('other').checked ? '\"' + String(document.getElementById('other_txt').value) + '\"' : '' 
+    };
+    options = {
+        method: "POST", 
+        body: JSON.stringify(request),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    };
+    fetch(apiURL, options).then((res) => {
+        if(res.ok) {
+            console.log("csv logged");
+        } else {
+            throw new Error("Error sending data");
+        }
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
 });
