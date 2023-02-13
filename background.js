e2444f4c81ac964bd03e2b69d1f91c1dd272ae99
David <daviddanelian3@gmail.com>
2023-02-13 13:35:19 -0500

    fixed issue with buffer

--- background.js
+++ background.js
@@ -1,5 +1,32 @@
-import { stat } from 'fs';
-import Buffer from './state-buffer';
+class Buffer {
+  constructor(size) {
+    this.size = size;
+    this.buffer = [];
+  }
+
+  add(element) {
+    this.buffer.push(element);
+    if (this.buffer.length > this.size) {
+      this.buffer.shift();
+    }
+  }
+
+  average() {
+    let sum = 0;
+    for (let i = 0; i < this.buffer.length; i++) {
+      sum += this.buffer[i];
+    }
+    return sum / this.buffer.length;
+  }
+
+  isFull() {
+    return this.buffer.length == this.size;
+  }
+
+  empty() {
+    this.buffer = []
+  }
+}
 
 const ACQUISITION = 0;
 const FOLLOWING = 1;
